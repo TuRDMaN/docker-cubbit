096557c919421cd62897e256cee0467d98a997ef
TuRDMaN <turdman@gmail.com>
2020-10-18 21:01:34 -0400

    Update startwm.sh

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -2,13 +2,18 @@
 
 DOWNDIR="/config/Downloads"
 RUNFILE="Cubbit-7.2.2.AppImage"
+RUNCMD="$DOWNDIR/$RUNFILE --appimage-extract-and-run &"
 
 /usr/bin/pulseaudio --start
 /usr/bin/startxfce4 > /dev/null 2>&1
 
-if [ mv /$RUNFILE $DOWNDIR/$RUNFILE ] ; then
-  $DOWNDIR/$RUNFILE --appimage-extract-and-run &
+if [ $DOWNDIR/$RUNFILE ] ; then
+  /bin/bash -c "$RUNCMD"
 else
-  wget "https://get.cubbit.io/desktop/linux/$RUNFILE" -O "$DOWNDIR/$RUNFILE"
-  $DOWNDIR/$RUNFILE --appimage-extract-and-run &
+  if [ mv /$RUNFILE $DOWNDIR/$RUNFILE ] ; then
+    /bin/bash -c "$RUNCMD"
+  else
+    wget "https://get.cubbit.io/desktop/linux/$RUNFILE" -O "$DOWNDIR/$RUNFILE"
+    /bin/bash -c "$RUNCMD"
+  fi  
 fi
